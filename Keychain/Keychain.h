0bcc90243695408c6d9a21e0829038c4507cf3d1
Tim Ellis <tim.ellis@accelo.com>
2019-11-29 10:13:39 +1100

    Initial commit

---
+++ Keychain/Keychain.h
@@ -0,0 +1,19 @@
+//
+//  Keychain.h
+//  Keychain
+//
+//  Created by Timothy Ellis on 29/11/19.
+//  Copyright © 2019 Tim Ellis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Keychain.
+FOUNDATION_EXPORT double KeychainVersionNumber;
+
+//! Project version string for Keychain.
+FOUNDATION_EXPORT const unsigned char KeychainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Keychain/PublicHeader.h>
+
+
